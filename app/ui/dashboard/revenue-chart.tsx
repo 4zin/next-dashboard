ec7a0c14abf582c38a35691c150ce4c95ec503b2
Camilo Rodelo <camilo_jrl@hotmail.com>
2024-01-27 20:02:40 -0500

    Chapter 9 static and dynamic rendering

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -31,7 +31,7 @@ export default async function RevenueChart({
       {/* NOTE: comment in this code when you get to this point in the course */}
 
       <div className="rounded-xl bg-gray-50 p-4">
-        <div className="m-2 mt-0 grid grid-cols-12 items-end gap-4 rounded-md bg-white sm:grid-cols-13 md:gap-4">
+        <div className="m-2 mt-0 grid grid-cols-12 items-end gap-4 rounded-md bg-white p-2 sm:grid-cols-13 md:gap-4">
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
